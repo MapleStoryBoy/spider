0300b42362fb86e8c4a0b522931338f32e4d791f
MapleStoryBoy <MapleStoryBoy@163.com>
2019-06-06 07:28:29 +0800

    机器学习

--- 机器学习/特征工程/特征抽取1.py
+++ 机器学习/特征工程/特征抽取1.py
@@ -1,35 +1,22 @@
 from sklearn.feature_extraction import DictVectorizer
 
 
-
-
-
-
-
-
-
-
-
-
-
 def dictvec():
     '''
     字典数据抽取
     :return: None
     '''
-    #实例化
+    # 实例化
     dict = DictVectorizer(sparse=False)
 
     # 调用fit_transform
-    data = dict.fit_transform([{'city': '北京','temperature': 100}, {'city': '上海','temperature':60}, {'city': '深圳','temperature': 30}])
+    data = dict.fit_transform(
+        [{'city': '北京', 'temperature': 100}, {'city': '上海', 'temperature': 60}, {'city': '深圳', 'temperature': 30}])
 
     print(dict.get_feature_names())
     print(data)
 
     return None
-
-
-
 
 
 if __name__ == '__main__':
